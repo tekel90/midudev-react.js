b7a3f8a60be3a22278c88c66da4a0d31284fb2a9
tekel90 <you@example.com>
2020-08-18 02:21:11 -0300

    cargando

--- giffy/src/components/ListOfGifs.js
+++ giffy/src/components/ListOfGifs.js
@@ -4,15 +4,20 @@ import getGifs from '../services/getGifs'
 
 export default function ListOfGifs ({params}){
     const {keyword} = params
+    const [loading, setLoading] = useState(false)
 
     const [gifs, setGifs] = useState([])
 
     useEffect(function() {
+        setLoading(true)
         getGifs({ keyword })
-        .then(gifs => setGifs(gifs))
+        .then(gifs => {
+            setGifs(gifs)
+            setLoading(false)
+        })
     }, [keyword]) 
 
-
+if (loading) return <i>Cargando...</i>
 
 
     return <div>
